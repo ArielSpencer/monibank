4ba4cc41c75dadcca76495dae26866eaecc9bace
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-03 13:01:49 -0300

    Implementar validação de CPF

---
+++ js/valida-cpf.js
@@ -0,0 +1,63 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log('cpf invalido')
+    } else {
+        console.log('cpf válido')
+    }
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    } // loop para pegar cada número do cpf * multiplicador e somar tudo
+
+    soma = (soma * 10) % 11; // modulo da divisão de 11 (resto da divisão por 11)
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    } // soma != 10 e 11 = primeiro numero verificador (final do cpf)
+
+    return soma != cpf[9]; // comparação com 9º digito do cpf
+}
+
+// validacao com com os 9 primeiros digitos + primeiro verificador:
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
+}
+// fim da validação
